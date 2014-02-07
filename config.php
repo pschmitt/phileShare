5279304f574d913b9741913ee1a49bb0db87cd98
Philipp Schmitt <philipp@schmitt.co>
2014-02-07 23:53:10 +0100

    Add default config file

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+
+return array(
+    'share_services' => array('twitter' => true, 'facebook' => true, 'google' => true, 'linkedin' => false),
+    'share_output' => 'link',
+    'share_class_prefix' => 'btn-',
+    'share_div_id' => 'share',
+);
